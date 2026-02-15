ddaf1ad9c9538da7bd4c7ca45d7fdb2a548c1873
Arpita Jain <arpitajain88888@gmail.com>
2026-02-15 14:34:28 +0000

    Coded UC2 to Render OOPS as Banner using new file

--- OOPSBannerAppUC2.java
+++ OOPSBannerAppUC2.java
@@ -1,22 +1,20 @@
 /**
  *  * OOPSBannerApp UC2 - Banner Display Application
-  * * This class demonstrates the creation of a visual banner displaying the word "OOPS"
-   * using ASCII art composed of asterisks (*) and spaces.
-    * * @author Developer
-     * @version 2.0
-      */
-      public class OOPSBannerAppUC2 {
+  * * @author Developer
+   * @version 2.0
+    */
+    public class OOPSBannerAppUC2 {
 
-          public static void main(String[] args) {
-                  // Constructing each line of the OOPS banner individually
-                          // Alignment: width of 9 characters per letter
-                                  
-                                          System.out.println("  *** " + " " + "  *** " + " " + "****** " + " " + " ***** ");
-                                                  System.out.println(" ** ** " + " " + " ** ** " + " " + " ** **" + " " + "** **");
-                                                          System.out.println("** **" + " " + "** **" + " " + " ** **" + " " + "** ");
-                                                                  System.out.println("** **" + " " + "** **" + " " + " ******" + " " + " ***** ");
-                                                                          System.out.println("** **" + " " + "** **" + " " + " ** " + " " + "     **");
-                                                                                  System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "** **");
-                                                                                          System.out.println("  *** " + " " + "  *** " + " " + " ** " + " " + " ***** ");
-                                                                                              }
-                                                                                              }
+        public static void main(String[] args) {
+                // Each line is a concatenation of the O, O, P, and S patterns
+                        // Total width: 9 chars per letter + padding spaces
+                                
+                                        System.out.println("  *** " + "  *** " + "****** " + " ***** ");
+                                                System.out.println(" ** ** " + " ** ** " + " ** ** " + "** ** ");
+                                                        System.out.println("** ** " + "** ** " + " ** ** " + "** ");
+                                                                System.out.println("** ** " + "** ** " + " ****** " + " ***** ");
+                                                                        System.out.println("** ** " + "** ** " + " ** " + "     ** ");
+                                                                                System.out.println(" ** ** " + " ** ** " + " ** " + "** ** ");
+                                                                                        System.out.println("  *** " + "  *** " + " ** " + " ***** ");
+                                                                                            }
+                                                                                            }
