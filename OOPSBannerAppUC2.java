cc9767a1ee6705886c499e7fde5b95fc471b33ba
Arpita Jain <arpitajain88888@gmail.com>
2026-02-15 14:27:53 +0000

    Coded UC2 to Render OOPS as Banner using new file

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,22 @@
+/**
+ *  * OOPSBannerApp UC2 - Banner Display Application
+  * * This class demonstrates the creation of a visual banner displaying the word "OOPS"
+   * using ASCII art composed of asterisks (*) and spaces.
+    * * @author Developer
+     * @version 2.0
+      */
+      public class OOPSBannerAppUC2 {
+
+          public static void main(String[] args) {
+                  // Constructing each line of the OOPS banner individually
+                          // Alignment: width of 9 characters per letter
+                                  
+                                          System.out.println("  *** " + " " + "  *** " + " " + "****** " + " " + " ***** ");
+                                                  System.out.println(" ** ** " + " " + " ** ** " + " " + " ** **" + " " + "** **");
+                                                          System.out.println("** **" + " " + "** **" + " " + " ** **" + " " + "** ");
+                                                                  System.out.println("** **" + " " + "** **" + " " + " ******" + " " + " ***** ");
+                                                                          System.out.println("** **" + " " + "** **" + " " + " ** " + " " + "     **");
+                                                                                  System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "** **");
+                                                                                          System.out.println("  *** " + " " + "  *** " + " " + " ** " + " " + " ***** ");
+                                                                                              }
+                                                                                              }
