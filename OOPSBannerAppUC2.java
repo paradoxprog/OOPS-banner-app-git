b3287c9ed6433113c77a31ba8f5c68f817fba63d
Arpita Jain <arpitajain88888@gmail.com>
2026-03-01 20:11:42 +0530

    Refactor banner display in OOPSBannerAppUC2
    Updated the banner display logic and removed comments.

--- OOPSBannerAppUC2.java
+++ OOPSBannerAppUC2.java
@@ -1,20 +1,15 @@
-/**
- *  * OOPSBannerApp UC2 - Banner Display Application
-  * * @author Developer
-   * @version 2.0
-    */
-    public class OOPSBannerAppUC2 {
+public class OOPSBannerAppUC2 {
+    public static void main(String[] args) {
 
-        public static void main(String[] args) {
-                // Each line is a concatenation of the O, O, P, and S patterns
-                        // Total width: 9 chars per letter + padding spaces
-                                
-                                        System.out.println("  *** " + "  *** " + "****** " + " ***** ");
-                                                System.out.println(" ** ** " + " ** ** " + " ** ** " + "** ** ");
-                                                        System.out.println("** ** " + "** ** " + " ** ** " + "** ");
-                                                                System.out.println("** ** " + "** ** " + " ****** " + " ***** ");
-                                                                        System.out.println("** ** " + "** ** " + " ** " + "     ** ");
-                                                                                System.out.println(" ** ** " + " ** ** " + " ** " + "** ** ");
-                                                                                        System.out.println("  *** " + "  *** " + " ** " + " ***** ");
-                                                                                            }
-                                                                                            }
+        // Each letter is 7 lines high, width ~9 chars
+
+        System.out.println("  *****  " + "  *****  " + "  *****  " + "  *****  ");
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **   ** ");
+        System.out.println("**     **" + "**     **" + "**     **" + "**      ");
+        System.out.println("**     **" + "**     **" + "*******  " + "  *****  ");
+        System.out.println("**     **" + "**     **" + "**       " + "      ** ");
+        System.out.println(" **   ** " + " **   ** " + "**       " + " **   ** ");
+        System.out.println("  *****  " + "  *****  " + "**       " + "  *****  ");
+
+    }
+}
